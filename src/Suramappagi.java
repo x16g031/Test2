f49ced5008143aff551beac2a907d9a9b3b5c824
WIN_NT\x16g031 <x16g031@chiba-fjb.ac.jp>
2017-12-01 10:42:29 +0900

    ０２

--- src/Suramappagi.java
+++ src/Suramappagi.java
@@ -4,7 +4,7 @@ public class Suramappagi {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 System.out.println("スラマッパギー");
-System.out.println("単純な馬鹿でありたい");
+System.out.println("シャケだー");
 	}
 
 }
